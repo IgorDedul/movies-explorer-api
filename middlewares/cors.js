bb95c331431970d1b1934b9af5a32b394d7c500f
IgorDedul <dedul_igor@mail.ru>
2023-09-12 22:53:12 +0300

    feat: add cors and logger

---
+++ middlewares/cors.js
@@ -0,0 +1,23 @@
+const ALLOWED_CORS = [
+  'http://localhost:3000',
+];
+const ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (ALLOWED_CORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
+  }
+
+  return next();
+};
