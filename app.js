9e12b3fba078853ae9956c772065540a9a0c8836
IgorDedul <dedul_igor@mail.ru>
2023-09-13 00:40:35 +0300

    feat: add controller movies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const router = require('express').Router();
 const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
